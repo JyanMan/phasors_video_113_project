b2b868ede870dbdc02695881f389d2a9ab62e993
JyanMan <196519339+JyanMan@users.noreply.github.com>
2025-12-20 14:21:47 +0800

    made a wrapper for ReplacementTransform to return new

--- temp.py
+++ temp.py
@@ -2,6 +2,14 @@ from manim import *
 import numpy as np
 import math
 
+
+# replace transform but return new
+# use to replace the old with new after animation
+def play_replace_trans_full(self: Scene, old: Mobject, new: Mobject, **kwargs):
+    self.play(ReplacementTransform(old, new, **kwargs))
+    return new
+
+
 class Test(Scene):
     def construct(self):
         WAIT_FOR_VO = 2.5
@@ -64,9 +72,11 @@ class Test(Scene):
                 color=GREEN
             ).move_to(i_text.get_left(), aligned_edge=LEFT)
 
-        v_phi = MathTex(r"\phi_v = 0", font_size=50, color=YELLOW).center()
+        v_phi = MathTex(r"\phi_v = 0", font_size=50, color=YELLOW)
+        def v_phi_init():
+            return v_phi.move_to(v_text.get_right()).shift(RIGHT*2)
         def v_phi_to_v_text():
-            v_text_phi = v_text_formula().get_part_by_tex(r'\phi_v')[0]
+            v_text_phi = v_text.get_part_by_tex(r'\phi_v')
             v_text_phi_center = v_text_phi.get_center()
             return MathTex(r"0", font_size=50, color=YELLOW).move_to(v_text_phi_center)
 
@@ -82,7 +92,7 @@ class Test(Scene):
         self.play(v_text.animate().center().to_edge(UP).to_edge(LEFT).shift(RIGHT))
         self.play(Uncreate(volt_wave_end))
 
-        self.play(ReplacementTransform(v_text, v_text_formula()))
+        v_text = play_replace_trans_full(self, v_text, v_text_formula())
         self.wait(WAIT_FOR_VO)
 
         self.play(Create(curr_wave_end))
@@ -91,26 +101,13 @@ class Test(Scene):
         self.play(Write(i_text_end_curr_wave()), run_time=0.5)
         self.play(i_text.animate().next_to(v_text, DOWN, aligned_edge=LEFT))
 
-        self.play(ReplacementTransform(i_text, i_text_formula()))
+        i_text = play_replace_trans_full(self, i_text, i_text_formula())
         self.play(Uncreate(curr_wave_end))
         self.wait(WAIT_FOR_VO)
 
         ###### CURRENT WAVE INITIAL ANIMATION  ######
-
         # phi of v(t) being zero
-        self.play(Write(v_phi))
-
+        self.play(Write(v_phi_init()))
         self.wait(WAIT_FOR_VO)
-
-        self.play(Transform(v_phi, v_phi_to_v_text()))
+        v_phi = play_replace_trans_full(self, v_phi, v_phi_to_v_text())
         self.play(FadeOut(v_phi))
-
-
-
-
-
-
-
-
-        
-
